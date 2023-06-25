37fc6baca64f9a4103263149d23fb0580023189c
Juan Salgadov <juanfelipesalgadovarelacole@gmail.com>
2023-06-25 13:30:29 -0500

    Buscador

---
+++ Buscador/App.py
@@ -0,0 +1,16 @@
+from Buscador import *
+from Cargo import *
+from Departamento import *
+from Oferta import *
+from Ubicacion import *
+from Usuario import *
+Usuario = ("313 4668992", "correo@gmail.com", "Calle 24")
+print (Usuario)
+Ubicacion = ("25", "754", "Cundinamarca", "Soacha")
+print (Ubicacion)
+Oferta = ("623478", "3", "25/06/2023", "25/07/2023")
+print (Oferta)
+Cargo = ("Requisitos para el cargo", "6 meses", "Contrato temporal", "Ubicacion:", Ubicacion, "7")
+print (Cargo)
+Departamento = ("Departamentos de informática y programación", "Tipo 1")
+print (Departamento)
